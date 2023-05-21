95b86399a9c4d5bbc0f00fd07e2ff300637a3609
rialgou <rialgou@gmail.com>
2023-05-21 17:16:26 -0400

    qlearning terminado

--- snake-ai-qlearning/agent.py
+++ snake-ai-qlearning/agent.py
@@ -15,7 +15,9 @@ class Agent:
 
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0 # randomness
+        self.epsilon = 1.0 # randomness
+        self.epsilon_discount = 0.9992
+
         self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         self.table = np.zeros((2,2,2,2,2,2,2,2,2,2,2,3))
@@ -64,27 +66,13 @@ class Agent:
             game.food.y > game.head.y  # food down
             ]
 
-        return np.array(state, dtype=int)
+        return tuple(map(int,state))
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
 
-    def train_long_memory(self):
-        if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
-        else:
-            mini_sample = self.memory
-
-        states, actions, rewards, next_states, dones = zip(*mini_sample)
-        self.trainer.train_step(states, actions, rewards, next_states, dones)
-        #for state, action, reward, nexrt_state, done in mini_sample:
-        #    self.trainer.train_step(state, action, reward, next_state, done)
-
-    def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        self.epsilon = 80 - self.n_games
+        #self.epsilon = 40 - self.n_games
         final_move = [0,0,0]
         # exploration
         if random.randint(0, 200) < self.epsilon:
@@ -108,7 +96,7 @@ def train():
     while True:
         # get old state
         state_old = agent.get_state(game)
-
+        print(state_old)
         # get move
         final_move = agent.get_action(state_old)
         
@@ -116,21 +104,16 @@ def train():
         reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
 
-        # train short memory
-        agent.train_short_memory(state_old, final_move, reward, state_new, done)
-
-        # remember
-        agent.remember(state_old, final_move, reward, state_new, done)
-
+        # Bellman Equation Update
+        agent.table[state_old][final_move] = (1 - LR) * agent.table[state_old][final_move] + LR * (reward + agent.gamma * max(agent.table[state_new]))
+        agent.epsilon = max(agent.epsilon*agent.epsilon_discount,0.001)
         if done:
             # train long memory, plot result
             game.reset()
             agent.n_games += 1
-            agent.train_long_memory()
 
             if score > record:
                 record = score
-                agent.model.save()
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
